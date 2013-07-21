2d523bc47b493c5d2dc07084694686caedfe0117
rahim <rahim@rahim-ub.fritz.box>
2013-07-21 22:18:58 +0200

    free mind for chpater1 is added.

---
+++ notes/hadoopBook/ch1.mm
@@ -0,0 +1,4 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1374436965307" ID="ID_1430259642" MODIFIED="1374437891961" TEXT="Intorduction"/>
+</map>
